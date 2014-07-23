50353ef8d5e717b09f92e90eb3499b73b0bff594
Sh4d <aniellob@gmail.com>
2014-07-23 15:35:01 +0200

    Update setup.py
    patched setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ def read(fname):
 
 setup(
     name = "pyadb",
-    version = "0.1.1",
+    version = "0.1.2",
     author = "Chema Garcia",
     author_email = "chema@safetybits.net",
     description = ("Simple python module to interact with the ADB tool"),
     license = "BSD",
     keywords = "python android adb",
-    url = "https://github.com/sch3m4/pyadb",
+    url = "https://github.com/shadeimi/pyadb",
     packages=['pyadb'],
     long_description=read('README'),
     classifiers=[
